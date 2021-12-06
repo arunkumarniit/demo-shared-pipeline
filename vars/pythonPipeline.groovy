0e262e9c1f01e8359a038da4939191b32b456925
arunkumarniit <arunkumar.niit@gmail.com>
2021-12-06 14:31:24 +0530

    updates

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -7,19 +7,33 @@ def call (body) {
    String ChangeLog = '';
    String RepoUrl = config.repoUrl;
    String RepoBranch = config.repoBranch;
+   String DockerFileFolder = config.dockerFileFolder;
+   String DockerRegistry = config.dockerRegistry;
+   String DockerRegistryCredentials = config.dockerRegistryCredentials;
 
    pipeline {
       agent any
       stages {
-         stage('Checkout') {
+        stage('Checkout') {
             steps {
-                 // Clean before build
+                // Clean before build
                 cleanWs();
                 echo "Workspace is ${pwd()}";
                 git branch: "${RepoBranch}", url: "${RepoUrl}" 
-              
+                
             }
-         } 
+        }   
+        stage('Docker Build') {
+            steps {
+                dir("${DockerFileFolder}\\Dockerfile") {
+                    powershell(script: """
+                        docker images -a
+                        docker build -t ${registry} .
+                        docker images -a 
+                        """)                         
+                }
+            }
+        }
       }
    }
 }
